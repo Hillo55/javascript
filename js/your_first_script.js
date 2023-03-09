cee8a10ceb9ed2eec51dca47cb913a014f0bcaed
Hillo55 <112930918+Hillo55@users.noreply.github.com>
2023-03-09 13:42:12 +0200

    lisäyksiä

---
+++ js/your_first_script.js
@@ -0,0 +1,16 @@
+
+var me= "Leevi;"
+var age = 17;
+var education = "ohjelmistokehittäjäksi";
+var hometown = "vihti"
+
+var whoami = "olen "+ me +". opiskelen "+ education +". ikä on "+ age +". Kotipaikkani on "+ hometown+".";
+
+alert(whoami);
+
+
+console.warn("WARNING STRANGER DANGER");
+console.log("Name:", age, education);
+console.error("ERROR ERROR ERROR");
+
+       
